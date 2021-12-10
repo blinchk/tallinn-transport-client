e9d1a69f33b0a105deb451d197195b7ba1b696b1
Nikolas Laus <bchik3@gmail.com>
2021-12-10 14:19:14 +0200

    Fix seconds coercions in Arrival model.

--- src/main/java/model/Arrival.java
+++ src/main/java/model/Arrival.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.util.Date;
 
@@ -9,6 +10,10 @@ public class Arrival {
     private final Date actual;
     private final Date expected;
     private final boolean isLowFloor;
+
+    private final int TIME_DIFFERENCE_MILLISECONDS_DELIMITER = 1000;
+    private final int REMAINING_TIME_MILLISECONDS_DELIMITER = -TIME_DIFFERENCE_MILLISECONDS_DELIMITER;
+    private final int SECONDS_DELIMITER = 60;
 
     public Arrival(Route route, Stop stop, Date actual, Date expected, boolean isLowFloor) {
         this.route = route;
@@ -23,26 +28,30 @@ public class Arrival {
     }
 
     public String toString() {
-        return String.format("Stop: %s | Route: %s %s \n- Expected: %s | Actual: %s | %s min (%s s) \n- %s",
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm");
+        return String.format("Stop: %s | Route: %s %s \n- Expected: %s | Actual: %s | %s min (%s min) \n- %s",
                 stop.getName(),
                 route.getRouteNumber(),
                 route.getDestination(),
-                expected,
-                actual,
+                dateFormat.format(expected),
+                dateFormat.format(actual),
                 getRemainingTimeInMinutes(),
-                getTimeDifferenceInSeconds(),
+                getTimeDifferenceInMinutes(),
                 route.getTransportType().toString());
     }
 
     public long getRemainingTimeInSeconds() {
-        return Math.abs( Date.from(Instant.now()).getTime() - actual.getTime() ) / 1000;
+        return ( Date.from(Instant.now()).getTime() - actual.getTime() ) / REMAINING_TIME_MILLISECONDS_DELIMITER;
     }
 
     public long getRemainingTimeInMinutes() {
-        return (int) (getRemainingTimeInSeconds() / 60);
+        return (int) (getRemainingTimeInSeconds() / SECONDS_DELIMITER);
     }
 
     public long getTimeDifferenceInSeconds() {
-        return ( actual.getTime() - expected.getTime() ) / 1000;
+        return ( actual.getTime() - expected.getTime() ) / TIME_DIFFERENCE_MILLISECONDS_DELIMITER;
+    }
+    public long getTimeDifferenceInMinutes() {
+        return (int) (getTimeDifferenceInSeconds() / SECONDS_DELIMITER);
     }
 }
