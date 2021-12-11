ce5b70e6ee1c78fc28da90c24f28972b5c3d2983
Nikolas Laus <bchik3@gmail.com>
2021-12-11 07:50:49 +0200

    Init JUnit tests.

--- src/main/java/me/laus/tallinn_transport/Main.java
+++ src/main/java/me/laus/tallinn_transport/Main.java
@@ -2,19 +2,12 @@ package me.laus.tallinn_transport;
 
 import me.laus.tallinn_transport.client.external.SiriClient;
 import me.laus.tallinn_transport.client.internal.SiriStopClient;
+import me.laus.tallinn_transport.util.FileSystemUtils;
 
 import java.nio.file.Paths;
 import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
-        String currentPath = Paths.get("").toAbsolutePath().toString();
-        String path = currentPath + "/src/main/resources/stops.txt";
-        var stopsClient = new SiriStopClient(path);
-        var stops = stopsClient.parse();
-        stops.forEach(stop -> System.out.println(stop.getSiriId() + " " + stop.getName() + " " + stop.getLocation()));
-        var stop = stops.stream().parallel().filter(_stop -> Objects.equals(_stop.getSiriId(), String.valueOf(1333))).findFirst();
-        var siriClient = new SiriClient();
-        siriClient.request(stop.orElseThrow(RuntimeException::new)).forEach(System.out::println);
     }
 }
