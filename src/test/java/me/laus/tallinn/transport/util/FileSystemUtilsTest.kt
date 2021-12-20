bc3099a4bd927ab63039ef7b325b1c10c445a680
Nikolas Laus <bchik3@gmail.com>
2021-12-21 00:10:30 +0200

    Convert tests to Kotlin.

--- src/test/java/me/laus/tallinn/transport/util/FileSystemUtilsTest.kt
+++ src/test/java/me/laus/tallinn/transport/util/FileSystemUtilsTest.kt
@@ -1,12 +1,12 @@
-package me.laus.tallinn.transport.util;
+package me.laus.tallinn.transport.util
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions
+import me.laus.tallinn.transport.util.FileSystemUtils
+import org.junit.jupiter.api.Test
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class FileSystemUtilsTest {
+internal class FileSystemUtilsTest {
     @Test
-    void stopFileIsNotNull() {
-        assertNotNull(FileSystemUtils.DEFAULT_STOPS_PATH);
+    fun stopFileIsNotNull() {
+        Assertions.assertNotNull(FileSystemUtils.DEFAULT_STOPS_PATH)
     }
 }
